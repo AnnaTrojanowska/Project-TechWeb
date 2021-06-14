6264db2275c0f968df87f9a9821b9773e490c474
AnnaTrojanowska <anna.trojanowska1@microsoft.wsei.edu.pl>
2021-06-14 18:53:57 +0200

    js

--- app.js
+++ app.js
@@ -6,3 +6,30 @@ function myFunction() {
       x.className = "nav-links";
     }
   }
+
+  const playButton = document.getElementById('play');
+  function togglePlay() {
+    if (video.paused || video.ended) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
+  playButton.addEventListener('click', togglePlay);
+  const playbackIcons = document.querySelectorAll('.playback-icons use');
+  function updatePlayButton() {
+    playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
+  }
+  video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+
+function updatePlayButton() {
+  playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
+
+  if (video.paused) {
+    playButton.setAttribute('data-title', 'Play (k)')
+  } else {
+    playButton.setAttribute('data-title', 'Pause (k)')
+  }
+}
